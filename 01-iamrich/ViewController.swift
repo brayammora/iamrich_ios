8130f5e57959f742f3b642e06ed7c0f5d9b5ba32
Brayam Alberto Mora Arias <bryanmora22@gmail.com>
2020-02-24 16:41:19 -0500

    first commit

---
+++ 01-iamrich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  01-iamrich
+//
+//  Created by Brayam Alberto Mora Arias on 24/02/20.
+//  Copyright © 2020 Brayam Alberto Mora Arias. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
